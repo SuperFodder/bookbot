717200130aa848d9571ddb353d7b591a43e168c8
SuperFodder <tyler.d.hardy@hotmail.com>
2025-10-25 23:17:06 -0400

    added import of sys and exception handling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_recurring_characters
+import sys
 
 def get_book_text(path_str):
     with open(path_str) as f:
@@ -7,16 +8,19 @@ def get_book_text(path_str):
         return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count -----------")
-    print(f"Found {get_num_words(book_text)} total words")
-    print("--------- Character Count -------")
-    dict_list = get_recurring_characters(book_text)
-    for item in dict_list:
-        ch = item["char"]
-        if ch.isalpha():
-            print(f"{ch}: {item['num']}")
+    try: 
+        book_text = get_book_text(sys.argv[1])
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count -----------")
+        print(f"Found {get_num_words(book_text)} total words")
+        print("--------- Character Count -------")
+        dict_list = get_recurring_characters(book_text)
+        for item in dict_list:
+            ch = item["char"]
+            if ch.isalpha():
+                print(f"{ch}: {item['num']}")
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
 
 main()
